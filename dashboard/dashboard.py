88a51cf16072c16f20600705d6c1ba8913aba173
Rafi Nanda Edtrian <rafinanda9@gmail.com>
2025-04-15 12:19:39 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,14 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Konfigurasi halaman
-st.set_page_config(page_title="Dashboard Bike Sharing", layout="centered")
-
-# Judul utama
-st.title("🚲 Dashboard Analisis Data Bike Sharing")
-st.markdown("Analisis data *Bike Sharing* untuk memahami pola penggunaan sepeda dan faktor-faktor yang memengaruhinya.")
-
-
 
 # ================== Footer ==================
 st.markdown("---")
