3f6d5ea50848cedaff24caea02bd7626a73bf21e
Rafi Nanda Edtrian <rafinanda9@gmail.com>
2025-04-21 01:15:54 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,10 +8,17 @@ st.title("Dashboard Analisis Data Bike Sharing")
 st.write("Analisis data Bike Sharing untuk memahami pola penggunaan sepeda.")
 
 # Membaca data
-hour_df = pd.read_csv('dashboard/hour_clean.csv')  # Pastikan file CSV berada dalam folder yang sama
+hour_df = pd.read_csv('dashboard/hour_clean.csv')
 
-# Mengelompokkan data dan menghitung jumlah penyewaan per jam
+# Mengelompokkan data dan menghitung jumlah penyewaan per jam untuk hari kerja dan akhir pekan
 hourly_rental_counts = hour_df.groupby(['hour', 'weekday'], observed=False)['count'].sum().reset_index()
+
+# Memfilter data untuk hari kerja (Senin-Jumat)
+weekday_data = hourly_rental_counts[hourly_rental_counts['weekday'].isin(['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'])]
+
+# Memfilter data untuk akhir pekan (Sabtu-Minggu)
+weekend_data = hourly_rental_counts[hourly_rental_counts['weekday'].isin(['Sabtu', 'Minggu'])]
+
 
 # Membuat line chart untuk distribusi penyewaan sepeda berdasarkan jam
 plt.figure(figsize=(12, 6))
