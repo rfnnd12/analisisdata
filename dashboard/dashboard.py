ab7e0c324b278793f80733908fa9129dcfb173d7
Rafi Nanda Edtrian <rafinanda9@gmail.com>
2025-04-21 00:53:31 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -16,18 +16,25 @@ selected_holiday = st.sidebar.multiselect("Pilih Hari Libur atau Hari Kerja:", [
 
 # Filter data berdasarkan pilihan dari sidebar
 filtered_df_season = hour_df[hour_df['season'].isin(selected_season)]  # Menggunakan .isin untuk beberapa musim
-filtered_df_holiday = filtered_df_season[filtered_df_season['holiday'] == (0 if selected_holiday == 'Bukan Libur' else 1)]
+
+# Memastikan hanya mengambil data untuk Hari Libur atau Hari Kerja yang dipilih
+if 'Hari Libur' in selected_holiday and 'Hari Kerja' in selected_holiday:
+    filtered_df_holiday = filtered_df_season
+elif 'Hari Libur' in selected_holiday:
+    filtered_df_holiday = filtered_df_season[filtered_df_season['holiday'] == 1]  # Ambil data hari libur
+else:
+    filtered_df_holiday = filtered_df_season[filtered_df_season['holiday'] == 0]  # Ambil data hari kerja
 
 # Mengelompokkan data dan menghitung rata-rata 'cnt'
 rata_rata_penyewaan = filtered_df_holiday.groupby(['season', 'holiday', 'workingday'])['count'].mean().reset_index()
 
 # Membuat bar chart
 plt.figure(figsize=(12, 6))
-sns.barplot(x='season', y='count', hue='workingday', data=rata_rata_penyewaan[rata_rata_penyewaan['holiday'] == 0])
+sns.barplot(x='season', y='count', hue='workingday', data=rata_rata_penyewaan)
 plt.title(f'Rata-rata Jumlah Penyewaan Sepeda per Musim (Hari Kerja vs. Hari Libur) untuk Musim {", ".join(map(str, selected_season))}')
 plt.xlabel('Musim')
 plt.ylabel('Rata-rata Jumlah Penyewaan')
-plt.legend(title='Tipe Hari', labels=['Hari Libur', 'Hari Kerja'])
+plt.legend(title='Tipe Hari', labels=['Hari Kerja', 'Hari Libur'])
 
 # Menampilkan plot di Streamlit
 st.pyplot(plt)
