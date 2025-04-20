42cba6e7b441dad8121232f85499058537d4b41e
Rafi Nanda Edtrian <rafinanda9@gmail.com>
2025-04-21 00:45:08 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,11 +5,6 @@ import matplotlib.pyplot as plt
 
 # Membaca data
 hour_df = pd.read_csv('dashboard/hour_clean.csv')  # Pastikan file CSV berada dalam folder yang sama
-
-# Sidebar untuk filter interaktif
-selected_season = st.sidebar.multiselect("Pilih Musim untuk Filter:", hour_df['season'].unique())
-selected_holiday = st.sidebar.selectbox("Pilih Hari Libur atau Hari Kerja:", ['Libur', 'Bukan Libur'])
-
 # Filter data berdasarkan pilihan dari sidebar
 filtered_df_season = hour_df[hour_df['season'].isin(selected_season)]  # Menggunakan .isin untuk beberapa musim
 filtered_df_holiday = filtered_df_season[filtered_df_season['holiday'] == (0 if selected_holiday == 'Bukan Libur' else 1)]
