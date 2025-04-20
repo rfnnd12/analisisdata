9df27690907c445f06f125a8d8b4a07340bd307f
Rafi Nanda Edtrian <rafinanda9@gmail.com>
2025-04-20 23:36:06 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,20 +6,13 @@ import matplotlib.pyplot as plt
 # Membaca data
 hour_df = pd.read_csv('dashboard/hour_clean.csv')  # Pastikan file CSV berada dalam folder yang sama
 
-st.write(hour_df.columns)
-
-# Sidebar for interactive filters
+# Sidebar untuk filter interaktif
 selected_season = st.sidebar.selectbox("Pilih Musim untuk Filter:", hour_df['season'].unique())
 selected_weather = st.sidebar.selectbox("Pilih Kondisi Cuaca untuk Filter:", hour_df['weather'].unique())
-selected_workingday = st.sidebar.selectbox("Pilih Hari Kerja atau Akhir Pekan:", ['Workingday', 'Weekend'])
 
-# Filter data based on selections from sidebar
+# Filter data berdasarkan pilihan dari sidebar
 filtered_df_season = hour_df[hour_df['season'] == selected_season]
 filtered_df_weather = filtered_df_season[filtered_df_season['weather'] == selected_weather]
-if selected_workingday == 'Workingday':
-    filtered_df_workingday = filtered_df_season[filtered_df_season['workingday'] == 1]
-else:
-    filtered_df_workingday = filtered_df_season[filtered_df_season['workingday'] == 0]
 
 # Business Question 1: Apa pengaruh Cuaca Terhadap Jumlah Penyewaan Sepeda?
 st.write("### Apakah hari dengan cuaca buruk seperti “Light Snow/Rain” atau “Severe Weather” menunjukkan penurunan signifikan pada jumlah penyewaan?")
@@ -35,12 +28,12 @@ st.pyplot(plt)
 
 # Business Question 2: Bagaimana tren penyewaan sepeda berbeda antara hari kerja (workingday) dan akhir pekan/hari libur?
 st.write("### Bagaimana tren penyewaan sepeda berbeda antara hari kerja (workingday) dan akhir pekan/hari libur?")
-hourly_rental_counts = filtered_df_workingday.groupby('hour')['count'].sum().reset_index()
+hourly_rental_counts = filtered_df_season.groupby('hour')['count'].sum().reset_index()
 
 # Plot
 plt.figure(figsize=(12, 6))
 sns.barplot(x='hour', y='count', data=hourly_rental_counts)
-plt.title(f'Pola Penyewaan Sepeda Berdasarkan Jam ({selected_workingday})')
+plt.title(f'Pola Penyewaan Sepeda Berdasarkan Jam ({selected_season})')
 plt.xlabel('Jam')
 plt.ylabel('Jumlah Penyewaan')
 st.pyplot(plt)
