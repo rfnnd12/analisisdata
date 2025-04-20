c1e1f9a6b5a955626d7bd52efebcaeed65c9acca
Rafi Nanda Edtrian <rafinanda9@gmail.com>
2025-04-20 23:24:03 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,23 +4,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Membaca data
-hour_df = pd.read_csv('dashboard/hour_clean.csv')  # Pastikan file CSV berada dalam folder yang sama
+hour_df = pd.read_csv('dashboard/hour_clean.csv')  
 
 # Menampilkan header
 st.title("Dashboard Analisis Data Bike Sharing")
 st.write("Analisis data Bike Sharing untuk memahami pola penggunaan sepeda.")
 
-st.write(hour_df.columns)
-
+# Group by weather and aggregate
 hour_df.groupby('weather').agg({
     'count': ['max', 'min', 'mean', 'sum']
 })
 
-plt.figure(figsize=(10,6))
+# Plot
+plt.figure(figsize=(10, 6))
 sns.boxplot(x='weather', y='count', data=hour_df)
 plt.title("Pengaruh Kondisi Cuaca Terhadap Jumlah Penyewaan Sepeda")
 plt.xlabel("Kondisi Cuaca")
 plt.ylabel("Jumlah Penyewaan")
-plt.show()
-
+st.pyplot(plt)
 
