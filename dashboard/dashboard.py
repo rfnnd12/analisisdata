7da94d50316127d43a294b8a924b1f4e19eea0ef
Rafi Nanda Edtrian <rafinanda9@gmail.com>
2025-04-21 00:46:07 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,9 +5,6 @@ import matplotlib.pyplot as plt
 
 # Membaca data
 hour_df = pd.read_csv('dashboard/hour_clean.csv')  # Pastikan file CSV berada dalam folder yang sama
-# Filter data berdasarkan pilihan dari sidebar
-filtered_df_season = hour_df[hour_df['season'].isin(selected_season)]  # Menggunakan .isin untuk beberapa musim
-filtered_df_holiday = filtered_df_season[filtered_df_season['holiday'] == (0 if selected_holiday == 'Bukan Libur' else 1)]
 
 # Mengelompokkan data dan menghitung rata-rata 'cnt'
 rata_rata_penyewaan = filtered_df_holiday.groupby(['season', 'holiday', 'workingday'])['count'].mean().reset_index()
