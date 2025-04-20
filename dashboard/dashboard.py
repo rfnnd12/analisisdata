21b0fd3ba1c0f4b8b3fadd2f20346f24bf48d46a
Rafi Nanda Edtrian <rafinanda9@gmail.com>
2025-04-21 01:14:23 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -13,14 +13,6 @@ hour_df = pd.read_csv('dashboard/hour_clean.csv')  # Pastikan file CSV berada da
 # Mengelompokkan data dan menghitung jumlah penyewaan per jam
 hourly_rental_counts = hour_df.groupby(['hour', 'weekday'], observed=False)['count'].sum().reset_index()
 
-# Memfilter data berdasarkan pilihan dari sidebar
-if selected_day_type == 'Hari Kerja':
-    # Memfilter data untuk hari kerja (Senin-Jumat)
-    plot_data = hourly_rental_counts[hourly_rental_counts['weekday'].isin(['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'])]
-else:
-    # Memfilter data untuk akhir pekan (Sabtu-Minggu)
-    plot_data = hourly_rental_counts[hourly_rental_counts['weekday'].isin(['Sabtu', 'Minggu'])]
-
 # Membuat line chart untuk distribusi penyewaan sepeda berdasarkan jam
 plt.figure(figsize=(12, 6))
 sns.lineplot(x='hour', y='count', data=plot_data, label=selected_day_type, color='blue')
