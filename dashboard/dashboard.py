897a0f426f7d83b1cf8df5bc7480beac19dcecdb
Rafi Nanda Edtrian <rafinanda9@gmail.com>
2025-04-21 00:47:42 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,6 +3,10 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Menampilkan header
+st.title("Dashboard Analisis Data Bike Sharing")
+st.write("Analisis data Bike Sharing untuk memahami pola penggunaan sepeda.")
+
 # Membaca data
 hour_df = pd.read_csv('dashboard/hour_clean.csv')  # Pastikan file CSV berada dalam folder yang sama
 
