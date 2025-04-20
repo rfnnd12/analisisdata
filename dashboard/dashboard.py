233a30056eb461f4918d96f346a930f470975df2
Rafi Nanda Edtrian <rafinanda9@gmail.com>
2025-04-21 00:44:17 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Membaca data
-hour_df = pd.read_csv('dashboard/hour_clean.csv')  # Pastikan file CSV berada dalam folder yang sama
+hour_df = pd.read_csv('dashboard/hour_clean.csv')
 
 # Sidebar untuk filter interaktif
 selected_season = st.sidebar.multiselect("Pilih Musim untuk Filter:", hour_df['season'].unique())
@@ -14,7 +14,7 @@ selected_weather = st.sidebar.multiselect("Pilih Kondisi Cuaca untuk Filter:", h
 rata_rata_penyewaan = hour_df.groupby(['season', 'holiday', 'workingday'])['count'].mean().reset_index()
 # Membuat bar chart
 plt.figure(figsize=(12, 6))
-sns.barplot(x='season', y='count', hue='workingday', data=rata_rata_penyewaan[rata_rata_penyewaan['holiday'] == 0])  # Filter untuk hari bukan liburan
+sns.barplot(x='season', y='count', hue='workingday', data=rata_rata_penyewaan[rata_rata_penyewaan['holiday'] == 0])  
 plt.title('Rata-rata Jumlah Penyewaan Sepeda per Musim (Hari Kerja vs. Hari Libur)')
 plt.xlabel('Musim')
 plt.ylabel('Rata-rata Jumlah Penyewaan')
