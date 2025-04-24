2ed4ec5652386360665eae30a2d055a7f5f71843
Rafi Nanda Edtrian <rafinanda9@gmail.com>
2025-04-25 00:12:44 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 # Menampilkan header
 st.title("Dashboard Analisis Data Bike Sharing")
